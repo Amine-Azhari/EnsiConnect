ae85bb9611ba902865d177db295f8236d5abb2ed
Zyad <zidelkaid@gmail.com>
2026-06-03 16:49:04 +0200

    resolve merge conflict

--- ensiconnect/lib/session.dart
+++ ensiconnect/lib/session.dart
@@ -1,1 +1,63 @@
-class Session {}
+import 'package:flutter/material.dart';
+
+class PostSessionPage extends StatefulWidget {
+  const PostSessionPage({super.key});
+
+  @override
+  State<PostSessionPage> createState() => _PostSessionPageState();
+}
+
+class _PostSessionPageState extends State<PostSessionPage> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+
+  void postSession() {
+    if (titleController.text.isNotEmpty &&
+        descriptionController.text.isNotEmpty) {
+      
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Session publiée !")),
+      );
+
+      Navigator.pop(context);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Remplis tous les champs")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Poster une session")),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: const InputDecoration(labelText: "Titre"),
+            ),
+
+            const SizedBox(height: 10),
+
+            TextField(
+              controller: descriptionController,
+              maxLines: 4,
+              decoration: const InputDecoration(labelText: "Description"),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: postSession,
+              child: const Text("Publier"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
