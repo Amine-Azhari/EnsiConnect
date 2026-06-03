5ab6872f0ee808c05f84c4392d1f2bbfc90be2cf
mrsalex <alexis.miras02@gmail.com>
2026-06-03 16:20:27 +0200

    FireBase

--- ensiconnect/lib/data_insert.dart
+++ ensiconnect/lib/data_insert.dart
@@ -4,8 +4,18 @@ class FirebaseDataService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Future<void> initialiserDonneesDeTest() async {
+
+    // On vérifie s'il y a déjà des étudiants
+    final snap = await _db.collection('Etudiant').limit(1).get();
+    
+    // Si la collection n'est pas vide, on arrête tout
+    if (snap.docs.isNotEmpty) {
+      print("⚠️ Données déjà présentes, pas besoin d'insérer.");
+      return; 
+    }
+    
     try {
-      // 1. Insertion des Étudiants
+      // Insertion des Étudiants
       // On stocke les références pour récupérer les IDs générés par Firebase
       DocumentReference refAlice = await _db.collection('Etudiant').add({
         'Nom': 'Dupont', 'Prenom': 'Alice', 'eMail': 'alice.dupont@uha.fr', 'Password': 'hash_password_123'
@@ -17,17 +27,17 @@ class FirebaseDataService {
         'Nom': 'El Fassi', 'Prenom': 'Sarah', 'eMail': 'sarah.elfassi@uha.fr', 'Password': 'hash_password_789'
       });
 
-      // 2. Insertion des Matières
+      // Insertion des Matières
       DocumentReference refMaths = await _db.collection('Matiere').add({'Nom': 'Algèbre Linéaire'});
       DocumentReference refInfo = await _db.collection('Matiere').add({'Nom': 'Programmation Orientée Objet'});
       DocumentReference refPhysique = await _db.collection('Matiere').add({'Nom': 'Thermodynamique'});
 
-      // 3. Insertion des Salles
+      // Insertion des Salles
       DocumentReference refSalleA = await _db.collection('Salle').add({'Nom': 'Salle TD 101'});
       await _db.collection('Salle').add({'Nom': 'Bibliothèque (Box 3)'});
       await _db.collection('Salle').add({'Nom': 'En ligne (Discord)'});
 
-      // 4. Insertion de la Session
+      // Insertion de la Session
       DocumentReference refSessionMaths = await _db.collection('Session').add({
         'MatiereID': refMaths.id,
         'SalleID': refSalleA.id,
@@ -37,23 +47,23 @@ class FirebaseDataService {
         'Heure_Fin': '16:00'
       });
 
-      // 5. Inscriptions (RejoindreSession)
+      // Inscriptions (RejoindreSession)
       await _db.collection('RejoindreSession').add({
         'EtudiantID': refAlice.id, 'SessionID': refSessionMaths.id, 'Date': '2026-06-05', 'Contenu': 'Super !'
       });
 
-      // 6. Évaluation
+      // Évaluation
       await _db.collection('Evaluation').add({
         'EvaluateurID': refAlice.id, 'EvalueID': refLucas.id, 'SessionID': refSessionMaths.id, 'Note': 5
       });
 
-      // 7. Messagerie
+      // Messagerie
       await _db.collection('Messagerie').add({
         'ExpediteurID': refLucas.id, 'DestinataireID': refSarah.id, 'SessionID': null, 
         'Date': '2026-06-06 18:30', 'Contenu': 'Salut Sarah, on peut se voir demain !'
       });
 
-      print('🚀 Données de test Firebase insérées avec succès !');
+      print('Données de test Firebase insérées avec succès !');
     } catch (e) {
       print('Erreur lors de l\'insertion Firebase : $e');
     }
--- ensiconnect/lib/main.dart
+++ ensiconnect/lib/main.dart
@@ -4,9 +4,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'auth.dart';
 import 'menu_principal.dart';
 import 'splash_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'data_insert.dart';
 
 // On garde le notifier global
 final ValueNotifier<bool> isDarkModeNotifier = ValueNotifier<bool>(false);
@@ -19,6 +19,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // On crée une instance du service et on lance l'insertion
+  final service = FirebaseDataService();
+  await service.initialiserDonneesDeTest();
   
   // On ouvre le stockage local
   final prefs = await SharedPreferences.getInstance();
