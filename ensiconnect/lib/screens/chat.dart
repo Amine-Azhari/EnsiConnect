72920f6a137053407f71717e1a659588ab3970f8
fontaine-r <fontaine.romainp@gmail.com>
2026-06-05 11:26:45 +0200

    Affichage des messages avec gestion mode clair/sombre

--- ensiconnect/lib/screens/chat.dart
+++ ensiconnect/lib/screens/chat.dart
@@ -14,8 +14,9 @@ class ChatPage extends StatefulWidget{
 }
 
 class _ChatPageState extends State<ChatPage> {
-
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final String currentUserId = "user1";
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +41,7 @@ class _ChatPageState extends State<ChatPage> {
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: textColor),
               ),
               const SizedBox(height: 10),
+
               ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
@@ -47,20 +49,21 @@ class _ChatPageState extends State<ChatPage> {
                 itemBuilder: (context, index) {
                   final convo = conversations[index];
 
-
+                  final otherUser = getOtherUser(convo.participants, currentUserId); 
 
                   return ListTile(
                     leading: CircleAvatar(
-                      child: Text(convo.participants[0][0].toUpperCase()),
+                      child: Text(otherUser[0].toUpperCase()),
                     ),
-                    title: Text(convo.participants[0]),
+                    title: Text(otherUser),
                     subtitle: Text(convo.lastMessage),
                     onTap: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => Conversation(
+                          builder: (_) => ConversationPage(
                             conversation:convo,
+                            currentUserId: currentUserId,
                           ),
                         ),
                       );
@@ -77,76 +80,7 @@ class _ChatPageState extends State<ChatPage> {
 }
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
 
-      appBar: AppBar(
-        title: Text(userName),
-        elevation: 0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        foregroundColor: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black87,
-      ),
-      body:Container(
-      ),
-      bottomNavigationBar: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            height: 61,
-            decoration: BoxDecoration(
-              color: Theme.of(context).brightness == Brightness.dark ? Colors.black87 : Colors.white,
-              borderRadius: BorderRadius.circular(35.0),
-              border: Border.all(
-                color: Colors.grey,
-                width: 2.0,
-              ),
-            ),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.face,
-                    color: EnsiConnectApp.ensisaBlue,
-                  ),
-                  onPressed: () {},
-                ),
-                Expanded(
-                  child: TextField(
-                    // onChanged: ,
-                    decoration: InputDecoration(
-                      hintText: "Votre message",
-                      hintStyle: TextStyle(
-                        color: EnsiConnectApp.ensisaBlue,
-                      ),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.attach_file,
-                    color: EnsiConnectApp.ensisaBlue,
-                  ),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  // onPressed : ,
-                  icon: Icon(
-                    Icons.send,
-                    color: EnsiConnectApp.ensisaBlue,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 final List<Conversation> conversations = [
   Conversation(
@@ -225,4 +159,119 @@ String getOtherUser(List<String> participants, String currentUserId) {
     (p) => p != currentUserId,
     orElse: () => participants.first,
   );
+}
+
+class ConversationPage extends StatefulWidget {
+  final Conversation conversation;
+  final String currentUserId;
+
+  const ConversationPage({
+    super.key,
+    required this.conversation,
+    required this.currentUserId,
+  });
+
+  @override
+  State<ConversationPage> createState() => _ConversationPageState();
+}
+
+class _ConversationPageState extends State<ConversationPage> {
+  final TextEditingController _controller = TextEditingController();
+    
+  @override
+  Widget build(BuildContext context) {
+    final otherUser = getOtherUser(widget.conversation.participants, widget.currentUserId);
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+
+      appBar: AppBar(
+        title: Text(otherUser),
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        foregroundColor: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black87,
+      ),
+
+      body:ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: widget.conversation.messages.length,
+        itemBuilder: (context, index) {
+          final msg = widget.conversation.messages[index];
+          final isMe = msg.senderId == widget.currentUserId;
+          return Align(
+            alignment:
+                isMe ? Alignment.centerRight : Alignment.centerLeft,
+            child: Container(
+              margin: const EdgeInsets.symmetric(vertical: 4),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: isMe ?
+                  Theme.of(context).brightness == Brightness.dark ?
+                 EnsiConnectApp.ensisaBlue :
+                 EnsiConnectApp.ensisaLightBlue :
+                 Theme.of(context).brightness == Brightness.dark ?
+                 Color.fromARGB(255, 72, 72, 72) :
+                 Color.fromARGB(255, 209, 209, 209),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text(msg.content),
+            ),
+          );
+        },
+      ),
+
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
+            height: 61,
+            decoration: BoxDecoration(
+              color: Theme.of(context).brightness == Brightness.dark ? Colors.black87 : Colors.white,
+              borderRadius: BorderRadius.circular(35.0),
+              border: Border.all(
+                color: Colors.grey,
+                width: 2.0,
+              ),
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.face,
+                    color: EnsiConnectApp.ensisaBlue,
+                  ),
+                  onPressed: () {},
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Votre message",
+                      hintStyle: TextStyle(
+                        color: EnsiConnectApp.ensisaBlue,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.attach_file,
+                    color: EnsiConnectApp.ensisaBlue,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.send,
+                    color: EnsiConnectApp.ensisaBlue,
+                  ),
+                  onPressed: () {_controller.clear();},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
