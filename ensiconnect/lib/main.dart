0fb82b054d046939d7a12fb34316a37740323c59
mrsalex <alexis.miras02@gmail.com>
2026-06-09 17:42:36 +0200

    nothing

--- ensiconnect/lib/main.dart
+++ ensiconnect/lib/main.dart
@@ -14,7 +14,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  
   // On crée une instance du service et on lance l'insertion
   final service = FirebaseDataService();
   await service.initialiserDonneesDeTest();
